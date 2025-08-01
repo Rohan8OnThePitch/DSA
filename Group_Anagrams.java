514b82806cba925ce2b8c320c3b81dedc5e0ccd1
ROHAN JOSEPH ALBEN <146981303+Rohan8OnThePitch@users.noreply.github.com>
2025-08-01 20:40:03 +0530

    Create Group_Anagrams.java

---
+++ Group_Anagrams.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = new String(ca);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
